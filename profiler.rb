e534aa54982bd071391b0288e0e913cce875a7ec
Pavel <32621060+VidgarVii@users.noreply.github.com>
2020-02-02 04:36:45 +0300

    Add code

---
+++ profiler.rb
@@ -0,0 +1,77 @@
+# frozen_string_literal: true
+
+# Profilers
+# gem 'stackprof'         https://github.com/tmm1/stackprof
+# gem 'ruby-prof'         https://ruby-prof.github.io/
+# gem 'benchmark-ips'
+# gem 'meta_request'
+# gem 'memory_profiler'   https://github.com/SamSaffron/memory_profiler
+
+# Performance.new(Class.new, :method, true).run_in(:stackprof)
+# Reports.ext_report(PARAMS)
+
+
+class Performance
+  PARAMS = { service_type: "videocomfort",
+             b_date: Date.parse("01.01.2019").beginning_of_day,
+             e_date: Date.parse("28.01.2020").end_of_day }
+
+
+  STACKPROF_MODE = :wall
+  UTILS = [:ruby_prof, :stackprof, :realtime, :memory_profiler]
+
+  def initialize(object, method, gc_off = false)
+    @object = object
+    @method = method
+    @gc_off = gc_off
+    RubyProf.measure_mode = RubyProf::WALL_TIME
+  end
+
+  def run_in(util)
+    raise 'Undefine method' unless UTILS.include?(util)
+
+    send util
+  end
+
+  private
+
+  def call_process
+    GC.disable if gc_off?
+    @object.send(@method, PARAMS)
+  end
+
+  def realtime
+    time = Benchmark.realtime do
+      call_process
+    end
+    puts "FINISHED IN #{time}"
+  end
+
+  # open in the kcachegrind
+  def ruby_prof
+    result = RubyProf.profile do
+      call_process
+    end
+
+    printer = RubyProf::CallTreePrinter.new(result)
+    printer.print(:path => "tmp/", :profile => "profile")
+  end
+
+  def stackprof
+    StackProf.run(mode: STACKPROF_MODE, raw: true, out: 'tmp/stackprof.dump', interval: 1000) do
+      call_process
+    end
+  end
+
+  def memory_profiler
+    report = MemoryProfiler.report do
+      call_process
+    end
+
+    report.pretty_print(file: 'tmp/memory_profile')
+  end
+
+  def gc_off?
+    @gc_off
+  end
+end
